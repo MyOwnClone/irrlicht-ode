ca058c4611f9ec33cf015805011ccf9567365ae8
Tomas Vymazal <tomas.vymazal@gmail.com>
2013-03-28 16:15:27 +0100

    dynamic shadows added to cube (without need of custom cube scene node)

--- irrlicht_ode/main.cpp
+++ irrlicht_ode/main.cpp
@@ -53,18 +53,21 @@ int main()
 
 	skin->setColor(EGUI_DEFAULT_COLOR::EGDC_BUTTON_TEXT , SColor(255, 255, 255, 255));
 
-	scene::ISceneNode* cubeSceneNode = smgr->addCubeSceneNode();
-
+	scene::ISceneNode* cubeSceneNode = smgr->addMeshSceneNode(smgr->getGeometryCreator()->createCubeMesh());
+	
 	if (cubeSceneNode)
 	{
 		cubeSceneNode->setMaterialTexture(0, driver->getTexture("t351sml.jpg"));
 		cubeSceneNode->setMaterialFlag(video::EMF_LIGHTING, false);
-		scene::ISceneNodeAnimator* anim = smgr->createFlyCircleAnimator(core::vector3df(0,0,30), 20.0f);
+		scene::ISceneNodeAnimator* anim = smgr->createFlyCircleAnimator(core::vector3df(0,30,0), 5.0f);
 		if (anim)
 		{
 			cubeSceneNode->addAnimator(anim);
 			anim->drop();
 		}
+
+		cubeSceneNode->setScale(vector3df(5, 5, 5));
+		((IMeshSceneNode*)cubeSceneNode)->addShadowVolumeSceneNode();
 	}
 
 	scene::ISceneNode* groundSceneNode = smgr->addCubeSceneNode();
@@ -82,6 +85,26 @@ int main()
 		groundSceneNode->setScale(core::vector3df(100, 0, 100));
 	}
 
+	irr::f32 lightColorKoef = 1.0f;
+
+	scene::ISceneNode*  lightNode = smgr->addLightSceneNode(0, core::vector3df(0,10,0), video::SColorf(lightColorKoef, lightColorKoef, lightColorKoef, 1.0f), 
+		800.0f); // radius
+
+	scene::ISceneNodeAnimator* animNode = NULL;
+	animNode = smgr->createFlyCircleAnimator (core::vector3df(0,300,0), 200.0f);
+	lightNode->addAnimator(animNode);
+	animNode->drop();
+
+	// attach billboard to light
+
+	lightNode = smgr->addBillboardSceneNode(lightNode, core::dimension2d<f32>(50, 50));
+	lightNode->setMaterialFlag(video::EMF_LIGHTING, false);
+	lightNode->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
+
+	lightNode->setMaterialTexture(0, driver->getTexture("particlewhite.bmp"));
+
+	smgr->setShadowColor(video::SColor(150,0,0,0));
+
 	while(device->run())
 	{
 		driver->beginScene(true, true, SColor(255, 0, 0, 0));
@@ -93,6 +116,9 @@ int main()
 		core::stringw strFps = L"FPS: ";
 		strFps += (s32)driver->getFPS();       
 		infotext->setText(strFps.c_str());
+
+		auto oldRotation = cubeSceneNode->getRotation();
+		cubeSceneNode->setRotation(vector3df(0.1, 0.1, 0.1)+oldRotation);
 	}
 
 	device->drop();
